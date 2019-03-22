8550157cad41f18c62c0f31017ff2309b230a2c3
Lee <lee.hearne@britanniapackaging.com>
2019-03-22 11:20:59 +0000

    Moved the data array build outside of the loop
    This array was being generated once per hash, but only needs to be done once, after all the hashes are read into the founds array.

--- submit.py
+++ submit.py
@@ -14,7 +14,8 @@ with open('token', 'r') as token_file:
 founds, data	= [], []
 for line in in_file:
     founds.append(line.rstrip())
-    data = {u"key": token, u"found": founds }
+
+data = {u"key": token, u"found": founds}
 
 response = requests.post(baseurl, json.dumps(data))
 print response.content
